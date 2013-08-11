619e9b220d66a261e7c6f86868573d6eb994af57
Jörg Richter <jri@deepamehta.de>
2013-08-11 15:44:56 +0200

    WebActivator fix: concurrent requests (#492).
    The Thymeleaf Context object (the view model) is created in **request-scope** (instead of WebActivator-global).
    This allows for
        a) concurrent requests to the same web application.
        b) deployment of more than one web application plugin at the same time.
    
    BREAKING CHANGES
    
    1. Your web application plugin's init() hook must call `initTemplateEngine()`.
    Formerly this was named `setupRenderContext()`.
    
    2. For page preparation call `setViewModel(name, value)`.
    Formerly you did `context.setVariable(name, value)`.
    
    3. Keep in mind a new Thymeleaf Context object is created with each request. For page preparation your plugin could still rely on data kept in the (authenticated) user session. You just need to transfer the required session data to the context (by calling setViewModel()) when preparing the page.
    
    See #492.

--- src/main/java/de/deepamehta/plugins/webactivator/WebActivatorPlugin.java
+++ src/main/java/de/deepamehta/plugins/webactivator/WebActivatorPlugin.java
@@ -1,8 +1,11 @@
 package de.deepamehta.plugins.webactivator;
 
 import de.deepamehta.core.osgi.PluginActivator;
+import de.deepamehta.core.service.event.PreProcessRequestListener;
 
 import com.sun.jersey.api.view.Viewable;
+// ### TODO: remove Jersey dependency. Move to JAX-RS 2.0.
+import com.sun.jersey.spi.container.ContainerRequest;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.TemplateProcessingParameters;
@@ -12,34 +15,88 @@ import org.thymeleaf.templateresolver.TemplateResolver;
 
 import org.osgi.framework.Bundle;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Path;
+
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 
 
-public class WebActivatorPlugin extends PluginActivator {
+// Note: although no REST service is provided the plugin is annotated as a root resource class.
+// Otherwise we can't receive JAX-RS context injections (HttpServletRequest).
+@Path("/webactivator")
+public class WebActivatorPlugin extends PluginActivator implements PreProcessRequestListener {
+
+    // ------------------------------------------------------------------------------------------------------- Constants
+
+    private static String ATTR_CONTEXT = "de.deepamehta.plugins.webactivator.Context";
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
-    protected Context context;
-
     private TemplateEngine templateEngine;
+
+    @javax.ws.rs.core.Context
+    private HttpServletRequest request;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+
+
+    // ********************************
+    // *** Listener Implementations ***
+    // ********************************
+
+
+
+    @Override
+    public void preProcessRequest(ContainerRequest req) {
+        // Note: we don't operate on the passed ContainerRequest but on the injected HttpServletRequest.
+        // At this spot we could use req.getProperties().put(..) instead of request.setAttribute(..) but at the other
+        // spots (setViewModel() and view()) we could not inject a ContainerRequest but only a javax.ws.rs.core.Request
+        // and Request does not provide a getProperties() method. And we neither can cast a Request into a
+        // ContainerRequest as the injected Request is actually a proxy object (in order to deal with multi-threading).
+        request.setAttribute(ATTR_CONTEXT, new Context());
+    }
+
+
+
+    // ===
+
+    public TemplateEngine getTemplateEngine() {
+        if (templateEngine == null) {
+            throw new RuntimeException("The template engine for " + this + " is not initialized. " +
+                "Don't forget calling initTemplateEngine() from the plugin's init() hook.");
+        }
+        return templateEngine;
+    }
+
+
 
     // ----------------------------------------------------------------------------------------------- Protected Methods
 
-    protected void setupRenderContext() {
-        //
+    protected void initTemplateEngine() {
         TemplateResolver templateResolver = new TemplateResolver();
         templateResolver.setResourceResolver(new BundleResourceResolver(bundle));
         //
-        this.templateEngine = new TemplateEngine();
+        templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
-        //
-        this.context = new Context();
-        context.setVariable("_templateEngine", templateEngine);
+    }
+
+    protected void setViewModel(String name, Object value) {
+        context().setVariable(name, value);
     }
 
     protected Viewable view(String templateName) {
-        return new Viewable(templateName, context);
+        return new Viewable(templateName, context());
+    }
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+
+    private Context context() {
+        return (Context) request.getAttribute(ATTR_CONTEXT);
     }
 
     // --------------------------------------------------------------------------------------------------- Inner Classes
@@ -58,8 +115,7 @@ public class WebActivatorPlugin extends PluginActivator {
         }
 
         @Override
-        public InputStream getResourceAsStream(TemplateProcessingParameters templateProcessingParameters,
-                                               String resourceName) {
+        public InputStream getResourceAsStream(TemplateProcessingParameters params, String resourceName) {
             try {
                 return bundle.getResource(resourceName).openStream();
             } catch (Exception e) {
